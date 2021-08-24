e05388449285e17cc53b8705c034cecc729677a9
moreshravana <moreshravana@localhost>
2021-08-24 13:06:05 +0530

    First Commit

---
+++ hb-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,46 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+ 
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();		
+		Session session = factory.getCurrentSession();
+		
+		try {						
+			Instructor tempInstructor = 
+					new Instructor("Anvesh", "Patel", "anvesh@gmail.com");			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.youtube.com",
+							"Guitar");		
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			session.beginTransaction();
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);								
+			session.getTransaction().commit();			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
